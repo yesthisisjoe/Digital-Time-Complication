495ae7ad621ac889c2a5ff745a16e9bb4952d33e
Joe Peplowski <joepeplowski@gmail.com>
2021-03-04 10:12:59 -0800

    Initial Commit

---
+++ Digital Time Complication WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  Digital Time Complication WatchKit Extension
+//
+//  Created by Joe Peplowski on 2021-03-04.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
