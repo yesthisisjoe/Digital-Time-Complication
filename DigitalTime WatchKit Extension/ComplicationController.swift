7f6a614507fec695737e1c5d73fdbffba6d05285
Joe Peplowski <joepeplowski@gmail.com>
2021-03-04 17:39:23 -0800

    Add SwiftLint and fix its issues

--- DigitalTime WatchKit Extension/ComplicationController.swift
+++ DigitalTime WatchKit Extension/ComplicationController.swift
@@ -7,9 +7,8 @@
 
 import ClockKit
 
-
 class ComplicationController: NSObject, CLKComplicationDataSource {
-    
+
     // MARK: - Complication Configuration
 
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
@@ -18,115 +17,121 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
             identifier: complicationDigitalTimeIdentifier,
             displayName: "Digital Time",
             supportedFamilies: CLKComplicationFamily.allCases)
-        
+
         let descriptors = [
             digitalTimeDesriptor
         ]
-        
+
         handler(descriptors)
     }
-    
+
     func handleSharedComplicationDescriptors(_ complicationDescriptors: [CLKComplicationDescriptor]) {
         // Do any necessary work to support these newly shared complication descriptors
     }
 
     // MARK: - Timeline Configuration
-    
+
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
         handler(.distantFuture)
     }
-    
-    func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
+
+    func getPrivacyBehavior(
+        for complication: CLKComplication,
+        withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
         handler(.showOnLockScreen)
     }
 
     // MARK: - Timeline Population
-    
+
+    // swiftlint:disable:next cyclomatic_complexity
     private func createTemplate(for complication: CLKComplication, onDate date: Date) -> CLKComplicationTemplate {
-        var template: CLKComplicationTemplate
         let timeProvider = CLKTimeTextProvider(date: date)
-        let circularTemplate = CLKComplicationTemplateGraphicCircularStackText(line1TextProvider: timeProvider, line2TextProvider: timeProvider)
-        
-        // TODO: Check the quality of each of these
+        let circularTemplate = CLKComplicationTemplateGraphicCircularStackText(
+            line1TextProvider: timeProvider,
+            line2TextProvider: timeProvider)
+
         switch complication.family {
         case .circularSmall:
-            template = CLKComplicationTemplateCircularSmallSimpleText(
-                textProvider: timeProvider)
+            return CLKComplicationTemplateCircularSmallSimpleText(textProvider: timeProvider)
         case .extraLarge:
-            template = CLKComplicationTemplateExtraLargeSimpleText(
-                textProvider: timeProvider)
+            return CLKComplicationTemplateExtraLargeSimpleText(textProvider: timeProvider)
         case .graphicBezel:
-            template = CLKComplicationTemplateGraphicBezelCircularText(
+            return CLKComplicationTemplateGraphicBezelCircularText(
                 circularTemplate: circularTemplate,
                 textProvider: timeProvider)
         case .graphicCircular:
-            template = circularTemplate
+            return circularTemplate
         case .graphicCorner:
-            template = CLKComplicationTemplateGraphicCornerStackText(
+            return CLKComplicationTemplateGraphicCornerStackText(
                 innerTextProvider: timeProvider,
                 outerTextProvider: timeProvider)
         case .graphicExtraLarge:
-            template = CLKComplicationTemplateGraphicExtraLargeCircularStackText(
+            return CLKComplicationTemplateGraphicExtraLargeCircularStackText(
                 line1TextProvider: timeProvider,
                 line2TextProvider: timeProvider)
         case .graphicRectangular:
-            template = CLKComplicationTemplateGraphicRectangularStandardBody(
+            return CLKComplicationTemplateGraphicRectangularStandardBody(
                 headerTextProvider: timeProvider,
                 body1TextProvider: timeProvider)
         case .modularLarge:
-            template = CLKComplicationTemplateModularLargeStandardBody(
+            return CLKComplicationTemplateModularLargeStandardBody(
                 headerTextProvider: timeProvider,
                 body1TextProvider: timeProvider)
         case .modularSmall:
-            template = CLKComplicationTemplateModularSmallSimpleText(
-                textProvider: timeProvider)
+            return CLKComplicationTemplateModularSmallSimpleText(textProvider: timeProvider)
         case .utilitarianLarge:
-            template = CLKComplicationTemplateUtilitarianLargeFlat(
-                textProvider: timeProvider)
+            return CLKComplicationTemplateUtilitarianLargeFlat(textProvider: timeProvider)
         case .utilitarianSmall:
-            // TODO: Is this valid?
-            template = CLKComplicationTemplateUtilitarianSmallFlat(
-                textProvider: timeProvider)
+            return CLKComplicationTemplateUtilitarianSmallFlat(textProvider: timeProvider)
         case .utilitarianSmallFlat:
-            template = CLKComplicationTemplateUtilitarianSmallFlat(
-                textProvider: timeProvider)
+            return CLKComplicationTemplateUtilitarianSmallFlat(textProvider: timeProvider)
         @unknown default:
             fatalError("Unknown complication family found.")
         }
-        
-        return template
     }
-    
-    private func createTimelineEntry(for complication: CLKComplication, onDate date: Date) -> CLKComplicationTimelineEntry {
+
+    private func createTimelineEntry(
+        for complication: CLKComplication,
+        onDate date: Date
+    ) -> CLKComplicationTimelineEntry {
         let template = createTemplate(for: complication, onDate: date)
         let timelineEntry = CLKComplicationTimelineEntry(date: date, complicationTemplate: template)
         return timelineEntry
     }
 
-    func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
+    func getCurrentTimelineEntry(
+        for complication: CLKComplication,
+        withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         let timelineEntry = createTimelineEntry(for: complication, onDate: Date())
         handler(timelineEntry)
     }
-    
-    func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
+
+    func getTimelineEntries(
+        for complication: CLKComplication,
+        after date: Date, limit: Int,
+        withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         var timelineEntries: [CLKComplicationTimelineEntry] = []
         var nextMinute: Date
-        
-        let barelyAfterDate = Date(timeIntervalSinceReferenceDate: date.timeIntervalSinceReferenceDate + Double.leastNormalMagnitude)
-        nextMinute = Date(timeIntervalSinceReferenceDate: (barelyAfterDate.timeIntervalSinceReferenceDate / 60.0).rounded(.up) * 60.0)
-        
+
+        let barelyAfterDate = Date(
+            timeIntervalSinceReferenceDate: date.timeIntervalSinceReferenceDate + Double.leastNormalMagnitude)
+        nextMinute = Date(
+            timeIntervalSinceReferenceDate: (barelyAfterDate.timeIntervalSinceReferenceDate / 60.0).rounded(.up) * 60.0)
+
         while limit > timelineEntries.count {
             let timelineEntry = createTimelineEntry(for: complication, onDate: nextMinute)
             timelineEntries.append(timelineEntry)
             nextMinute = Date(timeIntervalSinceReferenceDate: nextMinute.timeIntervalSinceReferenceDate + 60.0)
         }
-        
+
         handler(timelineEntries)
     }
 
     // MARK: - Sample Templates
-    
-    func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
+
+    func getLocalizableSampleTemplate(
+        for complication: CLKComplication,
+        withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         let template = createTemplate(for: complication, onDate: Date())
         handler(template)
     }
