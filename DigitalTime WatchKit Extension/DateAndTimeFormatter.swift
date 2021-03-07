86d19c7c5598b142c789b12a707fefec5c581940
Joe Peplowski <joepeplowski@gmail.com>
2021-03-06 20:31:42 -0800

    Add date and time format & skeleton for formatter

---
+++ DigitalTime WatchKit Extension/DateAndTimeFormatter.swift
@@ -0,0 +1,12 @@
+//
+//  DateAndTimeFormatter.swift
+//  DigitalTime WatchKit Extension
+//
+//  Created by Joe Peplowski on 2021-03-06.
+//
+
+import Foundation
+
+struct DateAndTimeFormatter {
+    
+}
