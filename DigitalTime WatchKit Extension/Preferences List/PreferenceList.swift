f9b9ab73a94b31a2f7f1e5cf845e0a60b3daa2d4
Joe Peplowski <joepeplowski@gmail.com>
2021-03-10 07:05:39 -0800

    Add title to Preference List

--- DigitalTime WatchKit Extension/Preferences List/PreferenceList.swift
+++ DigitalTime WatchKit Extension/Preferences List/PreferenceList.swift
@@ -9,19 +9,21 @@ import SwiftUI
 
 struct PreferenceList: View {
     var body: some View {
-        List {
-            PreferenceRow(
-                title: "Time Format",
-                exampleDate: Date(),
-                formats: DateAndTimeFormat.TimeFormatIdentifier.allCases)
-            PreferenceRow(
-                title: "Date Format (Short)",
-                exampleDate: Date(),
-                formats: DateAndTimeFormat.ShortDateFormatIdentifier.allCases)
-            PreferenceRow(
-                title: "Date Format (Long)",
-                exampleDate: Date(),
-                formats: DateAndTimeFormat.LongDateFormatIdentifier.allCases)
+        NavigationView {
+            List {
+                PreferenceRow(
+                    title: "Time Format",
+                    exampleDate: Date(),
+                    formats: DateAndTimeFormat.TimeFormatIdentifier.allCases)
+                PreferenceRow(
+                    title: "Date Format (Short)",
+                    exampleDate: Date(),
+                    formats: DateAndTimeFormat.ShortDateFormatIdentifier.allCases)
+                PreferenceRow(
+                    title: "Date Format (Long)",
+                    exampleDate: Date(),
+                    formats: DateAndTimeFormat.LongDateFormatIdentifier.allCases)
+            }.navigationTitle("Digital Time")
         }
     }
 }
